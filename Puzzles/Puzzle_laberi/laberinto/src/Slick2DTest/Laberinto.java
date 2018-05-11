2ca99b1673bcc72c825ca97b87f917c0a31da565
miguel-angel-criado <oscuroduswalker@gmail.com>
2018-05-11 18:48:39 +0200

    laberinto con menos fallos pero aun con fallos

--- Puzzles/Puzzle_laberi/laberinto/src/Slick2DTest/Laberinto.java
+++ Puzzles/Puzzle_laberi/laberinto/src/Slick2DTest/Laberinto.java
@@ -1,5 +1,6 @@
 package Slick2DTest;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -8,13 +9,13 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class Laberinto extends BasicGameState{
+public class Laberinto extends BasicGameState {
 
-     //Animation character, moveLeft, moveRight,moveUp,moveDown;
+    Animation character, moveLeft, moveRight,nomoveright,nomoveleft;
     Image conejo;
     private Image laberinto;
-    //int[] duration = {200,200};
-    private Input entrada;
+    int[] duration = {200,200,200,200};
+    private Principal principal;
     float characterPositionX = 580,  characterPositionY = 648;
     BasicGameState prevState = Principal.prevState;
     
@@ -25,9 +26,21 @@ public class Laberinto extends BasicGameState{
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-       this.laberinto = new Image("puzzle_laberi/final.tmx");
+       this.laberinto = new Image("Puzzle_laberi/final.tmx");
        this.conejo = new Image("res/conejo.png");
        Principal.prevState = this;
+       Image[] walkRight = {new Image("res/B.ANIM_1.png"),new Image("res/B.ANIM_2.png"),new Image("res/B.ANIM_3.png"),new Image("res/B.ANIM_4.png")}; 
+       Image[] walkLeft = {new Image("res/B.ANIM_1_OPUESTO.png"),new Image("res/B.ANIM_2_OPUESTO.png"),new Image("res/B.ANIM_3_OPUESTO.png"),new Image("res/B.ANIM_4_OPUESTO.png")};
+       Image[] nowalkright = {new Image("res/B.STANCE.png"),new Image("res/B.STANCE.png"),new Image("res/B.STANCE.png"),new Image("res/B.STANCE.png")};
+       Image[] nowalkleft = {new Image("res/B.STANCE_OPUESTO.png"),new Image("res/B.STANCE_OPUESTO.png"),new Image("res/B.STANCE_OPUESTO.png"),new Image("res/B.STANCE_OPUESTO.png")};
+        
+       moveRight = new Animation(walkRight, duration, true);
+       moveLeft = new Animation(walkLeft,duration,true);
+       nomoveright = new Animation(nowalkright,duration,true);
+       nomoveleft = new Animation(nowalkleft,duration,true);
+       
+       character = nomoveright;
+       
     }
 
     @Override
