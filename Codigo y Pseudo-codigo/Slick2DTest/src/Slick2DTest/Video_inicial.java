832e9ee8a11b83c9fa0c2a94ba2643022d08ddd3
Luismi12 <luis.oton@edu.uah.es>
2018-05-14 22:55:56 +0200

    src

--- Codigo y Pseudo-codigo/Slick2DTest/src/Slick2DTest/Video_inicial.java
+++ Codigo y Pseudo-codigo/Slick2DTest/src/Slick2DTest/Video_inicial.java
@@ -17,6 +17,7 @@ public class Video_inicial extends BasicGameState {
     private Animation playvideo,play;
     int[] duration = {4000,4000,5000,4000,4000,4000,4000,4000,2000,4000,6000,4000,2000,4000,4000,5000,3000};
     private Image fachada1,fachada2,fachada3,fachada4,fachada5,fachada6,fachada7,fachada8,fachada9,fachada10,fachada11,fachada12,fachada13,fachada14,fachada15,fachada16,fachada17;
+    private boolean playmusica = true;
     
     @Override
     public int getID() {
@@ -25,7 +26,7 @@ public class Video_inicial extends BasicGameState {
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        music = new Music("res/Cancion Intro Personajes.ogg");
+        music = new Music("res/musicaintro.ogg");
         fachada1 = new Image("res/fachada1.jpg");
         fachada2 = new Image("res/fachada2.jpg");
         fachada3 = new Image("res/fachada3.jpg");
@@ -52,7 +53,8 @@ public class Video_inicial extends BasicGameState {
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
-        this.play.draw();
+      
+       this.play.draw();
        
        play.stopAt(16);
         
@@ -60,10 +62,12 @@ public class Video_inicial extends BasicGameState {
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
-        if (true){
+       if (playmusica){
             this.music.play();
-        }
+            playmusica=false;
+  }
         if (play.isStopped()){
+            this.music.stop();
             sbg.enterState(1);
         }
     }
